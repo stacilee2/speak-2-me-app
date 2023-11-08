f5030317748b16d65f8e12e3a59c8ff1cf693fb5
Staci Rosales <stacileep2@gmail.com>
2023-11-08 07:29:11 -0600

    corrected home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,13 +8,13 @@ const Home = () => {
       <img src={Speak} alt="speakLogo" className="logo" style={{ width: '200px', }}/>
       <h3>This application offers real-time chat with other parents or therapists to get ideas for helping your child at home.</h3>
       <h4>What do we offer? </h4>
-        <h4>Video, information, tasks:</h4>
+        <h4>Video, information, tasks in the following areas:</h4>
         <div className='home-page-tasks'>
           <ul>-Articulation </ul>
           <ul>-Receptive Language</ul>
           <ul>-Expressive Language</ul>
           <ul>-Pragmatic Language</ul>
-          <ul>-Voice</ul>
+          <ul>-Fluency</ul>
         </div>
         <iframe
           title="What Does a Speech Language Therapist Do?"
